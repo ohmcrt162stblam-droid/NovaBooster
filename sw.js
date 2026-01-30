99c5396faeff6cea7d6d39b0006df31aa06256f6
ohmcrt162stblam-droid <ohmcrt162stblam@gmail.com>
2026-01-30 13:42:04 +0700

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE = 'nova-v1';
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/','index.html','css/style.css','js/booster.js'])));
+});
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
+
