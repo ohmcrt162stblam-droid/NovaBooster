24d12f8134b0e00c9eaf82e3b9ff5cb03c40c83c
ohmcrt162stblam-droid <ohmcrt162stblam@gmail.com>
2026-01-30 16:25:12 +0700

    Add icons and new apps to app links

--- js/app-links.js
+++ js/app-links.js
@@ -1,18 +1,24 @@
 const apps = [
-    { name: 'FB Lite', url: 'https://m.facebook.com' },
-    { name: 'X Lite', url: 'https://mobile.twitter.com' },
-    { name: 'YouTube', url: 'https://m.youtube.com' },
-    { name: 'TikTok', url: 'https://www.tiktok.com' },
-    { name: 'Google', url: 'https://www.google.com' },
-    { name: 'Maps', url: 'https://maps.google.com' }
+    // โซเชียลหลัก
+    { name: 'Instagram', url: 'https://www.instagram.com/', icon: '📸' },
+    { name: 'TikTok', url: 'https://www.tiktok.com/', icon: '🎵' },
+    { name: 'Facebook', url: 'https://m.facebook.com/', icon: '📘' },
+    
+    // เกมยอดฮิต (ลิงก์เปิดแอป หรือเข้า Play Store)
+    { name: 'ROV', url: 'https://play.google.com/store/apps/details?id=com.garena.game.kgth', icon: '⚔️' },
+    { name: 'Free Fire', url: 'https://play.google.com/store/apps/details?id=com.dts.freefireth', icon: '🔫' },
+    { name: 'Roblox', url: 'https://www.roblox.com/home', icon: '🟥' }
 ];
 
 const grid = document.getElementById('appGrid');
+// เคลียร์ของเก่าก่อน
+grid.innerHTML = '';
+
 apps.forEach(app => {
     const div = document.createElement('div');
     div.className = 'app-item';
-    div.innerHTML = `<div>⚡</div>${app.name}`;
+    // เพิ่มเอฟเฟกต์กดแล้วเด้ง
+    div.innerHTML = `<div class="app-icon">${app.icon}</div>${app.name}`;
     div.onclick = () => window.location.href = app.url;
     grid.appendChild(div);
 });
-
